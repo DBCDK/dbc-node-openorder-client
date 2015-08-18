3cbdc69bb7768285ae2f03842c8f721ee8946bba
Rikke Lose <rikkelose@dhcp314.dbc.dk>
2015-08-18 12:19:29 +0200

    json from xml + add tests

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,9 +1,58 @@
 'use strict';
 
 import {assert} from 'chai';
+import * as OpenOrder from '../client.js';
 
 describe('Dummy test', () => {
+
   it('makes a dummy test', () => {
     assert.equal(true, true);
   });
+
+  it('Assert positive result', function(done) {
+    this.timeout(3000);
+    setTimeout(done, 3000);
+    const config = {
+			endpoint: 'https://openorder.addi.dk/2.6.next',
+			user: '',
+      group: '',
+      password: '',
+      serviceRequester: 'PallesGavebod'
+		};
+
+    OpenOrder.init(config);
+    let result = OpenOrder.checkOrderPolicy({
+			pid: '870970-basis:28183488',
+			pickUpAgencyId: '710100'
+		});
+
+    result.then(function (policyResult) {
+      assert.equal(policyResult.checkOrderPolicyResponse.orderPossible[0], 'true');
+      done();
+    });
+  });
+
+  it('Assert negative result', function(done) {
+    this.timeout(3000);
+    setTimeout(done, 3000);
+    const config = {
+			endpoint: 'https://openorder.addi.dk/2.6.next',
+			user: '',
+      group: '',
+      password: '',
+      serviceRequester: 'PallesGavebod'
+		};
+
+    OpenOrder.init(config);
+    let result = OpenOrder.checkOrderPolicy({
+			pid: '870970-basis:28183488',
+			pickUpAgencyId: '772700'
+		});
+
+    result.then(function (policyResult) {
+      assert.equal(policyResult.checkOrderPolicyResponse.orderPossible[0], 'false');
+      done();
+    });
+  });
+
 });
